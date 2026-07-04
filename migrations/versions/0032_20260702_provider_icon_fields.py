3c2322fb9a855bf51b7a4cde420ad0778caf15c2
JiXu <jixu0090@gmail.com>
2026-07-05 01:03:34 +0800

    fix alembic revision chain

--- migrations/versions/0032_20260702_provider_icon_fields.py
+++ migrations/versions/0032_20260702_provider_icon_fields.py
@@ -1,7 +1,7 @@
 """provider icon metadata fields
 
-Revision ID: 0031
-Revises: 0030
+Revision ID: 0032
+Revises: 0031
 Create Date: 2026-07-02
 """
 
@@ -12,8 +12,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0031"
-down_revision: Union[str, None] = "0030"
+revision: str = "0032"
+down_revision: Union[str, None] = "0031"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
